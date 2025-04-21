a2ed7859eb472764714f52dd3c66f36b8f90db98
merie-san <davide.zhang2@gmail.com>
2025-04-21 17:49:03 +0200

    push from eclipse windows

--- hellodocker/src/main/java/com/examples/hellodocker/App.java
+++ hellodocker/src/main/java/com/examples/hellodocker/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for (int i = 0; i < 10; i++) {
+			System.out.println(i+"\tHello World!");
+		}
     }
 }
